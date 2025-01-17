77233224a75b256217e596a2333c9b1b3e8ee6fd
Corey Thompson <coreyjthompson@gmail.com>
2025-01-16 19:15:51 -0600

    inital commit after moving to new system

---
+++ Mayflower.Core/Infrastructure/Data/Migrations/20250117011301_FullCommit.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mayflower.Core.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FullCommit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "RecurrenceTheme",
+                keyColumn: "Id",
+                keyValue: 0,
+                column: "Name",
+                value: "One-time payment");
+
+            migrationBuilder.UpdateData(
+                table: "RecurrenceTheme",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Value",
+                value: "Day");
+
+            migrationBuilder.UpdateData(
+                table: "RecurrenceTheme",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Value",
+                value: "Week");
+
+            migrationBuilder.UpdateData(
+                table: "RecurrenceTheme",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Value",
+                value: "Month");
+
+            migrationBuilder.InsertData(
+                table: "RecurrenceTheme",
+                columns: new[] { "Id", "Name", "Value" },
+                values: new object[] { 4, "Yearly", "Year" });
+
+            migrationBuilder.UpdateData(
+                table: "ReminderTheme",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Bill");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "RecurrenceTheme",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.UpdateData(
+                table: "RecurrenceTheme",
+                keyColumn: "Id",
+                keyValue: 0,
+                column: "Name",
+                value: "No Recurrence");
+
+            migrationBuilder.UpdateData(
+                table: "RecurrenceTheme",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Value",
+                value: "Daily");
+
+            migrationBuilder.UpdateData(
+                table: "RecurrenceTheme",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Value",
+                value: "Weekly");
+
+            migrationBuilder.UpdateData(
+                table: "RecurrenceTheme",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Value",
+                value: "Monthly");
+
+            migrationBuilder.UpdateData(
+                table: "ReminderTheme",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Bill Payment");
+        }
+    }
+}
